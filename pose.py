1991038b2f00faa44688305bd23dba9b91c8a4df
Fajrul Falah <ffajrul55@gmail.com>
2022-07-11 08:27:26 +0100

    Update pose.py

--- pose.py
+++ pose.py
@@ -1,8 +1,11 @@
+from pydoc import cli
+from tkinter import RIGHT
 import cv2
 import mediapipe as mp
 import pydirectinput as pdi
 import numpy as np
 import time
+import subprocess
 
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
@@ -52,8 +55,15 @@ with mp_pose.Pose(
 # For webcam input:
 squat = False
 jump = False
+spacehold = False
 click = False
+click_hold = False
+right_click = False
 walk = False
+right = False
+left = False
+mouseup = False
+mousedown = False
 
 cap = cv2.VideoCapture(0)
 with mp_pose.Pose(
@@ -89,7 +99,8 @@ with mp_pose.Pose(
         wrist_y = (results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_WRIST].y + results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_WRIST].y) * image_height/2
         hip_y = (results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_HIP].y + results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_HIP].y) * image_height/2
         normalfactor = abs(results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_EAR].x - results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_EAR].x)
-        
+        hand_distance = abs(results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_WRIST].x-results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_WRIST].x)
+
         # ketika jongkok, pencet shift
         # jongkok dideteksi ketika posisi kepal tangan ada di bawah pinggul
         if wrist_y > hip_y and squat==False and walk==False:
@@ -102,12 +113,46 @@ with mp_pose.Pose(
 
         # ketika loncat, pencet spasi
         # loncat dideteksi ketika posisi kepal tangan berada di atas sikut
-        if elbow_y > wrist_y and jump==False and results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_WRIST].y < results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_ELBOW].y:
+        if elbow_y > wrist_y and jump==False and results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_WRIST].y < results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_ELBOW].y and results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_WRIST].y < results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_ELBOW].y and hand_distance > 0.1:
           jump = True
           pdi.press('space')
 
         elif elbow_y < wrist_y and jump==True:
           jump = False
+
+        # deteksi tangan sambung (control mouse atas bawah)
+        if hand_distance < 0.06 and elbow_y > wrist_y and mouseup == False:
+          mouseup = True
+          moveup = subprocess.Popen(["python", "mouseup.py"], stdin=None, stdout=None, stderr=None, close_fds=True)  
+        elif hand_distance < 0.06 and elbow_y < wrist_y and mousedown == False:
+          mousedown = True
+          movedown = subprocess.Popen(["python", "mousedown.py"], stdin=None, stdout=None, stderr=None, close_fds=True)
+        elif hand_distance > 0.06 and (mouseup == True or mousedown == True):
+          mouseup = False
+          mousedown = False
+          try:
+            pollup = moveup.poll()
+            if pollup is None:
+              moveup.terminate()
+            polldown = movedown.poll()
+            if polldown is None:
+              movedown.terminate()
+          except:
+            pass
+        '''
+        if hand_distance < 0.06 and elbow_y < wrist_y and mousedown == False:
+          mousedown = True
+          movedown = subprocess.Popen(["python", "mousedown.py"], stdin=None, stdout=None, stderr=None, close_fds=True)  
+        elif hand_distance > 0.06 and mousedown == True:
+          mouseup = False
+          mousedown = False
+          try:
+            poll = moveup.poll()
+            if poll is None:
+              movedown.terminate()
+          except:
+            pass
+        '''
 
         # deteksi klik
         # tangan kanan di atas dan kiri di bawah, lalu tangan kanan turun
@@ -116,27 +161,83 @@ with mp_pose.Pose(
 
         elif results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_WRIST].y > results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_ELBOW].y and click == True:
           click = False
-          pdi.click()
+          if click_hold == False: 
+            #pdi.leftClick()
+            pdi.mouseDown()
+            click_hold = True
+          else:
+            pdi.mouseUp()
+            click_hold = False
+        
+        # deteksi klik kanan
+        # tangan kiri di atas dan kanan di bawah, lalu tangan kiri turun
+        # print('kiri:',results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_WRIST].x)
+        # print('kanan:',results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_WRIST].x)
+        # print('kanan:',abs(results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_WRIST].x-results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_WRIST].x))
+        
+        if results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_WRIST].y < results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_ELBOW].y and results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_WRIST].y > results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_ELBOW].y:
+          right_click = True 
 
-        # kaki naik, pencet w
+        elif results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_WRIST].y > results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_ELBOW].y and right_click == True:
+          right_click = False
+          pdi.rightClick()
+        
+        # deteksi hold click
+        # tangan kiri di atas dan kanan di bawah, lalu tangan kiri turun
+        '''
+        if results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_WRIST].y < results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_ELBOW].y and results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_WRIST].y > results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_ELBOW].y:
+          click = True
+
+        elif results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_WRIST].y > results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_ELBOW].y and click == True:
+          click = False
+          pdi.click(button=RIGHT)
+        '''
+
+        # kaki kanan naik, pencet w
         if results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_ANKLE].y < (results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_ANKLE].y + results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_KNEE].y)/2 and walk == False:
           walk = True
-          pdi.keyDown('w')
           pdi.keyUp('shift')
+          pdi.keyDown('w')
         elif results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_ANKLE].y > (results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_ANKLE].y + results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_KNEE].y)/2 and walk == True:
           walk = False
           pdi.keyUp('w')
         
+        # kaki kiri naik, pencet spasi hold
+        if results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_ANKLE].y < (results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_ANKLE].y + results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_KNEE].y)/2 and spacehold == False:
+          spacehold = True
+          pdi.keyUp('shift')
+          pdi.keyDown('space')
+        elif results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_ANKLE].y > (results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_ANKLE].y + results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_KNEE].y)/2 and spacehold == True:
+          spacehold = False
+          pdi.keyUp('space')
+        
         # tangan kanan lurus, mouse gerak ke kanan
-        if results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_ELBOW].y < (results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_SHOULDER].y + normalfactor) and results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_ELBOW].y > (results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_SHOULDER].y - normalfactor):
-          pdi.moveRel(100,0,2)
+        if results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_ELBOW].y < (results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_SHOULDER].y + normalfactor) and results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_ELBOW].y > (results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_SHOULDER].y - normalfactor) and right ==False:
+          right = True
+          moveright = subprocess.Popen(["python", "mouseright.py"], stdin=None, stdout=None, stderr=None, close_fds=True)
           #print('kanan')
+        elif results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_ELBOW].y > (results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_SHOULDER].y + normalfactor) and right == True:
+          right = False
+          try:
+            poll = moveright.poll()
+            if poll is None:
+              moveright.terminate()
+          except:
+            pass
         
         # tangan kiri lurus, mouse gerak ke kiri
-        if results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_ELBOW].y < (results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_SHOULDER].y + normalfactor) and results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_ELBOW].y > (results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_SHOULDER].y - normalfactor):
-          pdi.moveRel(-100,0,2)
+        if results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_ELBOW].y < (results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_SHOULDER].y + normalfactor) and results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_ELBOW].y > (results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_SHOULDER].y - normalfactor) and left == False:
+          left = True
+          moveleft = subprocess.Popen(["python", "mouseleft.py"], stdin=None, stdout=None, stderr=None, close_fds=True)
           #print('kiri')
-
+        elif results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_ELBOW].y > (results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_SHOULDER].y + normalfactor) and left == True:
+          left = False
+          try:
+            poll = moveleft.poll()
+            if poll is None:
+              moveleft.terminate()
+          except:
+            pass
 
     # Flip the image horizontally for a selfie-view display.
     cv2.imshow('MediaPipe Pose', cv2.flip(image, 1))
